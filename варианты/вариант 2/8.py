4ab78e10f53b88930060cdf6edf25ec554f9377a
ASTRO333 <alexgerik2007@gmail.com>
2025-04-06 21:35:33 +0300

    *

--- варианты/вариант 2/8.py
+++
@@ -1,8 +0,0 @@
-n = 0
-for p1 in 'АПРСУ':
-    for p2 in 'АПРСУ':
-        for p3 in 'АПРСУ':
-            s = p1 + p2 + p3
-            n+=1
-            if s[0] == 'Р':
-                print(n)
--- варианты/вариант 3/5.py
+++
@@ -1,9 +0,0 @@
-for n in range(100,1000):
-    n1 = (n//10%10)*(n//100)
-    n2 = (n//10%10)*(n%10)
-    if n1 > n2:
-        r = str(n1) + str(n2)
-    else:
-        r = str(n2) + str(n1)
-    if r == '123':
-        print(n)
